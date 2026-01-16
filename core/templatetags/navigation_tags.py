e61919f36b0fb1abeed0e6608fb789f1056c6c37
vitaly <50383391+Neikow@users.noreply.github.com>
2026-01-16 14:28:52 +0100

    post cr

--- core/templatetags/navigation_tags.py
+++ core/templatetags/navigation_tags.py
@@ -6,4 +6,17 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
-    return Site.find_for_request(context["request"]).root_page
+    request = context.get("request")
+
+    if request is not None:
+        site = Site.find_for_request(request)
+        if site is not None:
+            return site.root_page
+
+    default_site = (
+        Site.objects.filter(is_default_site=True).first() or Site.objects.first()
+    )
+    if default_site is not None:
+        return default_site.root_page
+
+    return None
