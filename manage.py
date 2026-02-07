a23c9fb59255ae0b5ae3dd5d3f779f56cd74610a
vitaly <50383391+Neikow@users.noreply.github.com>
2026-02-07 21:57:12 +0100

    add DJANGO_SETTINGS_MODULE default back

--- manage.py
+++ manage.py
@@ -3,12 +3,14 @@
 
 import os
 import sys
+from dotenv import load_dotenv
 
 
 def main() -> None:
+    load_dotenv()
     """Run administrative tasks."""
     if not os.environ.get("DJANGO_SETTINGS_MODULE"):
-        raise Exception("DJANGO_SETTINGS_MODULE not set.")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "urban_platform.settings.dev")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
