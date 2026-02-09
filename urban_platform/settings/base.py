40eeb30c35bf8f55b291757553b41c5ef31841c2
dmassif <dmassif@centrale-marseille.fr>
2026-02-09 13:11:05 +0100

    Add reset password in login modal

--- urban_platform/settings/base.py
+++ urban_platform/settings/base.py
@@ -179,6 +179,8 @@ WAGTAILSEARCH_BACKENDS = {
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 WAGTAILADMIN_BASE_URL = os.environ.get("BASE_URL", "http://localhost:8000")
 
+# Disable Wagtail's default password reset functionality
+WAGTAIL_PASSWORD_RESET_ENABLED = False
 # Allowed file extensions for documents in the document library.
 # This can be omitted to allow all files, but note that this may present a security risk
 # if untrusted users are allowed to upload files -
