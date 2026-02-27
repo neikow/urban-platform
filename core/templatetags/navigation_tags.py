1237e1c6338d71ed8649740b7fe7fb026ce6bc4c
vitaly <50383391+Neikow@users.noreply.github.com>
2026-02-27 11:25:41 +0100

    active link in header

--- core/templatetags/navigation_tags.py
+++ core/templatetags/navigation_tags.py
@@ -1,6 +1,12 @@
+from dataclasses import dataclass
+from django.utils.translation import gettext_lazy as _
+from django_stubs_ext import StrOrPromise
+
 from django import template
 from django.template.context import BaseContext
 from wagtail.models import Site, Page
+from wagtail.templatetags.wagtailcore_tags import pageurl
+
 from legal.models import LegalIndexPage
 
 register = template.Library()
@@ -32,3 +38,41 @@ def get_legal_pages(context: BaseContext) -> list[Page]:
     if legal_index:
         return legal_index.get_children().live().in_menu()
     return []
+
+
+@dataclass
+class MenuItem:
+    title: StrOrPromise
+    url: str
+    is_active: bool
+
+
+@register.simple_tag(takes_context=True)
+def get_navigation_menu_items(
+    context: BaseContext,
+) -> list[MenuItem]:
+    root = get_site_root(context)
+    request = context["request"]
+    if not request:
+        return []
+
+    home_url = pageurl(context, root) or "/"
+
+    menu_items = [
+        MenuItem(
+            title=_("Home"),
+            url=home_url,
+            is_active=request.path == home_url,
+        )
+    ]
+    for page in root.get_children().live().in_menu():
+        page_url = pageurl(context, page)
+        menu_items.append(
+            MenuItem(
+                title=page.title,
+                url=page_url or "#",
+                is_active=request.path == page_url,
+            )
+        )
+
+    return menu_items
