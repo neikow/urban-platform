2a0e7c178b4e7e09ad8dfc952eaaebb6e85cc208
Ludivine <ludivine.mauget@centrale-marseille.fr>
2026-03-02 14:48:47 +0100

    toc update and optional + nowrap vote btn

--- core/toc.py
+++ core/toc.py
@@ -13,22 +13,33 @@ class TableOfContentsItem:
 
 
 def generate_header_ids(block: StreamValue.StreamChild) -> None:
-    html = block.value.source
-
-    soup = BeautifulSoup(html, "html.parser")
-    for header in soup.find_all(["h2", "h3", "h4"]):
-        header_id = slugify(header.get_text())
-        header["id"] = header_id
-    block.value.source = str(soup)
+    if block.block_type == "text":
+        html = block.value.source
+        soup = BeautifulSoup(html, "html.parser")
+        for header in soup.find_all(["h2", "h3", "h4"]):
+            header_id = slugify(header.get_text())
+            header["id"] = header_id
+        block.value.source = str(soup)
+    elif block.block_type == "rich_text":
+        html = block.value["text"].source
+        soup = BeautifulSoup(html, "html.parser")
+        for header in soup.find_all(["h2", "h3", "h4"]):
+            header_id = slugify(header.get_text())
+            header["id"] = header_id
+        block.value["text"].source = str(soup)
 
 
 def get_table_of_contents(content: StreamValue) -> list[TableOfContentsItem]:
     toc: list[TableOfContentsItem] = []
 
     for block in content:
+        html = None
         if block.block_type == "text":
             html = block.value.source
+        elif block.block_type == "rich_text":
+            html = block.value["text"].source
 
+        if html:
             soup = BeautifulSoup(html, "html.parser")
             for header in soup.find_all(["h2", "h3", "h4"]):
                 level = int(header.name[1])
