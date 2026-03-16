addd8d3889039acc9ff34570e8da794560c2dd87
vitaly <50383391+Neikow@users.noreply.github.com>
2026-03-16 15:03:56 +0100

    mypy
    Took 19 seconds

--- core/templatetags/django_settings.py
+++ core/templatetags/django_settings.py
@@ -4,7 +4,7 @@ register = template.Library()
 
 
 @register.simple_tag()
-def get_setting(name):
+def get_setting(name: str) -> str:
     from django.conf import settings
 
     return getattr(settings, name, "")
